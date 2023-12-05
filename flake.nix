5ce3ac69d5c4a403b46755785fd73c4922d0f977
Mirko Lenz <mirko@mirkolenz.com>
2023-12-05 12:33:08 +0100

    fix: update nixpkgs channel to backport PR

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/backport-269324-to-release-23.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     system-manager = {
