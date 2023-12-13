41cdc924c4f1aba137176ba6afd49600e87c8dff
Mirko Lenz <mirko@mirkolenz.com>
2023-12-13 20:32:04 +0100

    fix(ssh): set printmotd to no

--- modules/banner.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}: {
-  environment.etc."banner".text = ''
-    Welcome to our GPU server! Please check out the documentation:
-    https://github.com/wi2trier/gpu-server
-
-  '';
-}
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -2,6 +2,12 @@
   environment.etc."ssh/sshd_config.d/nixos.conf".text = ''
     PermitRootLogin no
     X11Forwarding no
-    Banner /etc/banner
+    Banner /etc/sshd_banner.txt
+    PrintMotd no
+  '';
+  environment.etc."sshd_banner.txt".text = ''
+    Welcome to our GPU server! Please check out the documentation:
+    https://github.com/wi2trier/gpu-server
+
   '';
 }
