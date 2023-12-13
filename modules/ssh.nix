e09448d3525c06487ddd106944cac18fb5271d1f
Mirko Lenz <mirko@mirkolenz.com>
2023-12-13 20:26:20 +0100

    fix: sshd config

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,7 +1,7 @@
 {lib, ...}: {
-  environment.etc."ssh/ssh_config.d/nixos.conf".text = ''
-    PermitRootLogin no;
-    X11Forwarding no;
-    Banner /etc/banner;
+  environment.etc."ssh/sshd_config.d/nixos.conf".text = ''
+    PermitRootLogin no
+    X11Forwarding no
+    Banner /etc/banner
   '';
 }
