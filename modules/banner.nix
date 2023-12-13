1f56dd50e8b59be444937e90eff6b2355e30cae5
Mirko Lenz <mirko@mirkolenz.com>
2023-12-13 20:53:37 +0100

    fix: use ssd banner again

--- modules/banner.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  environment.etc."motd".text = ''
-    Welcome to our GPU server! Please check out our documentation:
-
-    https://github.com/wi2trier/gpu-server
-
-    If you encounter any problems, feel free to open an issue on GitHub or contact your supervisor.
-  '';
-}
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,6 +1,18 @@
-{lib, ...}: {
-  environment.etc."ssh/sshd_config.d/nixos.conf".text = ''
-    PermitRootLogin no
-    X11Forwarding no
-  '';
+{lib, ...}: let
+  bannerFile = "sshd-banner.txt";
+in {
+  environment.etc = {
+    "ssh/sshd_config.d/nixos.conf".text = ''
+      PermitRootLogin no
+      X11Forwarding no
+      Banner /etc/${bannerFile}
+    '';
+    ${bannerFile}.text = ''
+      Welcome to our GPU server! Please check out our documentation:
+
+      https://github.com/wi2trier/gpu-server
+
+      If you encounter any problems, feel free to open an issue on GitHub or contact your supervisor.
+    '';
+  };
 }
