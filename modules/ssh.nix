ad398e81b609d944a1aa804f5857b1b28dfd104e
Mirko Lenz <mirko@mirkolenz.com>
2023-12-13 20:58:43 +0100

    fix: update banner text

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -8,11 +8,10 @@ in {
       Banner /etc/${bannerFile}
     '';
     ${bannerFile}.text = ''
-      Welcome to our GPU server! Please check out our documentation:
+      Welcome to our GPU server! Please check out the documentation.
+      If you encounter any problems, feel free to open an issue on GitHub or contact your supervisor.
 
       https://github.com/wi2trier/gpu-server
-
-      If you encounter any problems, feel free to open an issue on GitHub or contact your supervisor.
 
     '';
   };
