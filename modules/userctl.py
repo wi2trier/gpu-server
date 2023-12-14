e31a9f89fdda0083ce0bf50e68255f897dd4e22b
Mirko Lenz <mirko@mirkolenz.com>
2023-12-14 15:22:45 +0100

    feat(userctl): output messages

--- modules/userctl.py
+++ modules/userctl.py
@@ -19,7 +19,10 @@ def app_callback():
         raise typer.Abort()
 
 
-def run_cmd(cmd: Sequence[str], input: str | None = None) -> str:
+def run_cmd(msg: str | None, cmd: Sequence[str], input: str | None = None) -> str:
+    if msg:
+        typer.echo(msg)
+
     cmd_kwargs = {}
 
     if input:
@@ -74,11 +77,17 @@ def add(
     if quota:
         zfs_args["quota"] = quota
 
+    typer.echo()
     # https://manpages.ubuntu.com/manpages/jammy/en/man8/zfs.8.html
-    run_cmd(["zfs", "create", *zfs_options(zfs_args), homedir_zfs(user)])
+    run_cmd(
+        "Creating ZFS dataset...",
+        ["zfs", "create", *zfs_options(zfs_args), homedir_zfs(user)],
+    )
 
-    # copy skeleton
-    run_cmd(["cp", "--recursive", "/etc/skel/.", homedir(user)])
+    run_cmd(
+        "Copying skeleton to home directory...",
+        ["cp", "--recursive", "/etc/skel/.", homedir(user)],
+    )
 
     useradd_args: list[str] = [
         "--home-dir",
@@ -93,22 +102,36 @@ def add(
         useradd_args += ["--expiredate", expire_date.strftime(DATE_FORMAT)]
 
     # https://manpages.ubuntu.com/manpages/jammy/en/man8/useradd.8.html
-    run_cmd(["useradd", *useradd_args, user])
+    run_cmd(
+        "Creating user...",
+        ["useradd", *useradd_args, user],
+    )
 
-    # set permissions
-    run_cmd(["chown", "-R", f"{user}:{user}", homedir(user)])
-    run_cmd(["chmod", "750", homedir(user)])
+    run_cmd(
+        "Changing owner of home directory...",
+        ["chown", "-R", f"{user}:{user}", homedir(user)],
+    )
+    run_cmd(
+        "Setting permissions of home directory...",
+        ["chmod", "750", homedir(user)],
+    )
 
     # https://manpages.ubuntu.com/manpages/jammy/en/man8/chpasswd.8.html
-    run_cmd(["chpasswd"], input=f"{user}:{password}")
+    run_cmd(
+        "Generating initial password...",
+        ["chpasswd"],
+        input=f"{user}:{password}",
+    )
 
     # https://manpages.ubuntu.com/manpages/jammy/en/man1/passwd.1.html
-    run_cmd(["passwd", "--expire", user])
+    run_cmd(
+        "Forcing password change on first login...",
+        ["passwd", "--expire", user],
+    )
 
     typer.echo("Login data for new user:")
     typer.echo(f"Username: {user}")
-    typer.echo(f"Password: {password}")
-    typer.echo("The initial password has to be changed on the first login.")
+    typer.echo(f"Initial password: {password}")
 
 
 @app.command()
@@ -128,10 +151,16 @@ def remove(
         userdel_args.append("--force")
 
     # https://manpages.ubuntu.com/manpages/jammy/en/man8/userdel.8.html
-    run_cmd(["userdel", *userdel_args, user])
+    run_cmd(
+        "Removing user...",
+        ["userdel", *userdel_args, user],
+    )
 
     if not keep_home:
-        run_cmd(["zfs", "destroy", "--force", homedir_zfs(user)])
+        run_cmd(
+            "Destroying ZFS dataset...",
+            ["zfs", "destroy", "--force", homedir_zfs(user)],
+        )
 
 
 @app.command()
@@ -153,7 +182,10 @@ def edit(
 
     if usermod_args:
         # https://manpages.ubuntu.com/manpages/jammy/en/man8/useradd.8.html
-        run_cmd(["usermod", *usermod_args, user])
+        run_cmd(
+            "Modifying user...",
+            ["usermod", *usermod_args, user],
+        )
 
     zfs_args: dict[str, str] = {}
 
@@ -161,7 +193,10 @@ def edit(
         zfs_args["quota"] = quota
 
     if zfs_args:
-        run_cmd(["zfs", "set", *zfs_options(zfs_args), homedir_zfs(user)])
+        run_cmd(
+            "Setting ZFS quota...",
+            ["zfs", "set", *zfs_options(zfs_args), homedir_zfs(user)],
+        )
 
 
 if __name__ == "__main__":
