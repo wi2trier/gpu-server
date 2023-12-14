56bb385f740f61bed662cdab0637ef62f6335c3d
Mirko Lenz <mirko@mirkolenz.com>
2023-12-14 10:31:53 +0100

    fix: allow output folder for container builders

--- modules/images.nix
+++ modules/images.nix
@@ -8,10 +8,11 @@
       # https://github.com/NixOS/nixpkgs/blob/aa9d4729cbc99dabacb50e3994dcefb3ea0f7447/pkgs/build-support/docker/default.nix#L490
       name = "build-container";
       text = ''
-        if [ "$#" -ne 1 ]; then
-          echo "Usage: $0 IMAGE_NAME" >&2
+        if [ "$#" -lt 1 ]; then
+          echo "Usage: $0 IMAGE_NAME [OUTPUT_FOLDER=.]" >&2
           exit 1
         fi
+        cd "$2" || exit 1
         BUILDER_SCRIPT="$1-builder.sh"
         ${lib.getExe pkgs.nix} build -o "$BUILDER_SCRIPT" "github:wi2trier/gpu-server#image-$1"
         ./"$BUILDER_SCRIPT" \
@@ -23,11 +24,12 @@
     (pkgs.writeShellApplication {
       name = "build-apptainer";
       text = ''
-        if [ "$#" -ne 1 ]; then
-          echo "Usage: $0 IMAGE_NAME" >&2
+        if [ "$#" -lt 1 ]; then
+          echo "Usage: $0 IMAGE_NAME [OUTPUT_FOLDER=.]" >&2
           exit 1
         fi
-        build-container "$1"
+        cd "$2" || exit 1
+        build-container "$1" # we already changed to the output folder
         ${lib.getExe pkgs.apptainer} build "$1.sif" "docker-archive:$1.tar.gz"
         rm "$1.tar.gz"
       '';
