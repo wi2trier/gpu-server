4a88dcbe5326b4aa4cfe9f884c997ddc661f0fad
Mirko Lenz <mirko@mirkolenz.com>
2024-11-21 10:51:57 +0100

    fix(systemd): enable strict shell checks

--- modules/systemd.nix
+++ modules/systemd.nix
@@ -1,39 +1,43 @@
 { lib, pkgs, ... }:
 {
-  systemd.services = {
-    update-system = {
-      enable = true;
-      startAt = "*-*-* 04:00:00";
-      script = ''
-        ${lib.getExe pkgs.nix} run github:wi2trier/gpu-server
-        ${lib.getExe pkgs.nix} run github:wi2trier/gpu-server#setup
-      '';
-      serviceConfig.Type = "oneshot";
-    };
-    link-cuda =
-      let
-        source = "/usr/lib/x86_64-linux-gnu";
-        target = "/run/opengl-driver/lib";
-      in
-      {
+  systemd = {
+    enableStrictShellChecks = true;
+    services = {
+      update-system = {
         enable = true;
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-        };
-        wantedBy = [ "system-manager.target" ];
+        startAt = "*-*-* 04:00:00";
         script = ''
-          # Remove old links
-          ${lib.getExe' pkgs.coreutils "rm"} -rf ${target}
-          ${lib.getExe' pkgs.coreutils "mkdir"} -p ${target}
-          # Link all .so files specified in Apptainer
-          ${lib.getExe pkgs.gnugrep} '\.so$' ${pkgs.apptainer}/etc/apptainer/nvliblist.conf | while read file
-          do
-            ${lib.getExe' pkgs.coreutils "ln"} -s ${source}/$file.* ${target}
-          done
-          # Remove broken links
-          ${lib.getExe pkgs.findutils} -L ${target} -maxdepth 1 -type l -delete
+          ${lib.getExe pkgs.nix} run github:wi2trier/gpu-server
+          ${lib.getExe pkgs.nix} run github:wi2trier/gpu-server#setup
         '';
+        serviceConfig.Type = "oneshot";
       };
+      link-cuda =
+        let
+          source = "/usr/lib/x86_64-linux-gnu";
+          target = "/run/opengl-driver/lib";
+        in
+        {
+          enable = true;
+          serviceConfig = {
+            Type = "oneshot";
+            RemainAfterExit = true;
+          };
+          wantedBy = [ "system-manager.target" ];
+          script = ''
+            # Remove old links
+            rm -rf ${target}
+            mkdir -p ${target}
+
+            # Link all .so files specified in Apptainer
+            grep '\.so$' ${pkgs.apptainer}/etc/apptainer/nvliblist.conf | while read file; do
+              ln -s ${source}/$file.* ${target}
+            done
+
+            # Remove broken links
+            find -L ${target} -maxdepth 1 -type l -delete
+          '';
+        };
+    };
   };
 }
