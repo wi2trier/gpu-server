1b5e1b47089c2b1f62348984539e82960fa9b7c5
Mirko Lenz <mirko@mirkolenz.com>
2024-12-08 20:49:30 +0100

    fix(open-webui): add home env var

--- modules/llm.nix
+++ modules/llm.nix
@@ -11,5 +11,11 @@
     enable = true;
     host = "0.0.0.0";
     port = 8000;
+    environment = {
+      SCARF_NO_ANALYTICS = "True";
+      DO_NOT_TRACK = "True";
+      ANONYMIZED_TELEMETRY = "False";
+      HOME = ".";
+    };
   };
 }
