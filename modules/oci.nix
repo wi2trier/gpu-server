33c36cb7e07bc739760e178895f3a33531c88f43
Mirko Lenz <mirko@mirkolenz.com>
2024-12-10 11:38:27 +0100

    fix: temporarily disable oci containers

--- modules/oci.nix
+++ modules/oci.nix
@@ -34,28 +34,28 @@ in
     };
   };
   virtualisation.oci-containers.containers = {
-    ollama = mkContainer {
-      image = "docker.io/ollama/ollama:latest";
-      volumes = [
-        "/var/lib/ollama-oci:/root/.ollama"
-      ];
-      extraOptions = [
-        "--device"
-        "nvidia.com/gpu=4,5,6,7"
-      ];
-    };
-    open-webui = mkContainer {
-      image = "ghcr.io/open-webui/open-webui:latest";
-      ports = [
-        "3000:8080"
-      ];
-      volumes = [
-        "/var/lib/open-webui-oci:/app/backend/data"
-      ];
-      extraOptions = [
-        "--add-host"
-        "host.docker.internal:host-gateway"
-      ];
-    };
+    # ollama = mkContainer {
+    #   image = "docker.io/ollama/ollama:latest";
+    #   volumes = [
+    #     "/var/lib/ollama-oci:/root/.ollama"
+    #   ];
+    #   extraOptions = [
+    #     "--device"
+    #     "nvidia.com/gpu=4,5,6,7"
+    #   ];
+    # };
+    # open-webui = mkContainer {
+    #   image = "ghcr.io/open-webui/open-webui:latest";
+    #   ports = [
+    #     "3000:8080"
+    #   ];
+    #   volumes = [
+    #     "/var/lib/open-webui-oci:/app/backend/data"
+    #   ];
+    #   extraOptions = [
+    #     "--add-host"
+    #     "host.docker.internal:host-gateway"
+    #   ];
+    # };
   };
 }
