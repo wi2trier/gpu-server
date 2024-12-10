5fa65499e3e275eca42ae5cd955e211defcad0dc
Mirko Lenz <mirko@mirkolenz.com>
2024-12-10 12:03:45 +0100

    fix(oci): chown volume mounts

--- modules/oci.nix
+++ modules/oci.nix
@@ -24,38 +24,32 @@ let
 in
 {
   systemd.tmpfiles.settings.oci-containers = {
-    "/var/lib/ollama-oci".d = {
-      user = "containers";
-      group = "containers";
-    };
-    "/var/lib/open-webui-oci".d = {
-      user = "containers";
-      group = "containers";
-    };
+    "/var/lib/ollama-oci".d = { };
+    "/var/lib/open-webui-oci".d = { };
   };
   virtualisation.oci-containers.containers = {
-    # ollama = mkContainer {
-    #   image = "docker.io/ollama/ollama:latest";
-    #   volumes = [
-    #     "/var/lib/ollama-oci:/root/.ollama"
-    #   ];
-    #   extraOptions = [
-    #     "--device"
-    #     "nvidia.com/gpu=4,5,6,7"
-    #   ];
-    # };
-    # open-webui = mkContainer {
-    #   image = "ghcr.io/open-webui/open-webui:latest";
-    #   ports = [
-    #     "3000:8080"
-    #   ];
-    #   volumes = [
-    #     "/var/lib/open-webui-oci:/app/backend/data"
-    #   ];
-    #   extraOptions = [
-    #     "--add-host"
-    #     "host.docker.internal:host-gateway"
-    #   ];
-    # };
+    ollama = mkContainer {
+      image = "docker.io/ollama/ollama:latest";
+      volumes = [
+        "/var/lib/ollama-oci:/root/.ollama:U"
+      ];
+      extraOptions = [
+        "--device"
+        "nvidia.com/gpu=4,5,6,7"
+      ];
+    };
+    open-webui = mkContainer {
+      image = "ghcr.io/open-webui/open-webui:latest";
+      ports = [
+        "3000:8080"
+      ];
+      volumes = [
+        "/var/lib/open-webui-oci:/app/backend/data:U"
+      ];
+      extraOptions = [
+        "--add-host"
+        "host.docker.internal:host-gateway"
+      ];
+    };
   };
 }
