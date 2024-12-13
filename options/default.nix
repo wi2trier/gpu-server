5ae6ffa551f4554d3e8e0acd9ea87a2ce071a2fe
Mirko Lenz <mirko@mirkolenz.com>
2024-12-13 08:13:15 +0100

    fix: use unstable nixos modules for ollama and open-webui

--- options/default.nix
+++ options/default.nix
@@ -2,13 +2,14 @@
   lib,
   lib',
   nixosModulesPath,
+  inputs,
   config,
   ...
 }:
 {
   imports = [
-    "${nixosModulesPath}/services/misc/ollama.nix"
-    "${nixosModulesPath}/services/misc/open-webui.nix"
+    "${inputs.nixpkgs-unstable}/nixos/modules/services/misc/ollama.nix"
+    "${inputs.nixpkgs-unstable}/nixos/modules/services/misc/open-webui.nix"
     "${nixosModulesPath}/virtualisation/containers.nix"
     "${nixosModulesPath}/virtualisation/oci-containers.nix"
   ] ++ (lib'.flocken.getModules ./.);
