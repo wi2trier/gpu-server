0ee8b51c2e4f53ce35f455e2fc8516cf6df8bdee
Mirko Lenz <mirko@mirkolenz.com>
2025-01-24 08:03:15 +0100

    chore(llm): add list of models

--- modules/llm.nix
+++ modules/llm.nix
@@ -3,6 +3,19 @@
   services.ollama = {
     enable = true;
     acceleration = "cuda";
+    loadModels = [
+      "aya-expanse:32b"
+      "aya-expanse:8b"
+      "command-r:35b"
+      "command-r7b:7b"
+      "deepseek-r1:14b"
+      "deepseek-r1:70b"
+      "gemma2:27b"
+      "gemma2:9b"
+      "llama3.2:3b"
+      "llama3.3:70b"
+      "phi4:14b"
+    ];
   };
   services.open-webui = {
     enable = true;
