0f4fc500130a67a885e7561e3bfaa51d827417f6
Mirko Lenz <mirko@mirkolenz.com>
2025-05-19 08:03:50 +0200

    feat(deps): update to nixos 25.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     system-manager = {
