f902c946931b19dc2fa399f0618b9dc14774c4da
Mirko Lenz <mirko@mirkolenz.com>
2025-08-06 14:40:03 +0200

    chore(deps): update ollama

--- overlays/packages/ollama-bin.nix
+++ overlays/packages/ollama-bin.nix
@@ -6,13 +6,13 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "ollama";
-  version = "0.11.0";
+  version = "0.11.2";
 
   # https://github.com/ollama/ollama/releases/latest
   # copy the hash for asset `ollama-linux-amd64.tgz` from the release page
   src = fetchurl {
     url = "https://github.com/ollama/ollama/releases/download/v${version}/ollama-linux-amd64.tgz";
-    hash = "sha256:6627e9898ab0e7924e4bdda05a7c41c66eaa23404ef237ae952c1ae2c86129de";
+    hash = "sha256:8fc97cb9b91f116f234d7db1aba77a0f38e9b62879dc161fa825eb4b2ef9b859";
   };
 
   sourceRoot = ".";
