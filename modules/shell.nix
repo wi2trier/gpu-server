f14a2a4132361dbb28fb6e16311ab39d40457c76
Mirko Lenz <mirko@mirkolenz.com>
2026-02-09 22:13:15 +0100

    fix(shell): update env var setup

--- modules/shell.nix
+++ modules/shell.nix
@@ -3,30 +3,20 @@
 { pkgs, lib, ... }:
 let
   envVars = {
-    CUDA_VISIBLE_DEVICES = "100";
-    CUDA_DEVICE_ORDER = "PCI_BUS_ID";
   };
 in
 {
   environment.etc = {
     # Make sure that no user uses all GPUs accidentally
     # If device order is not set, nvidia-smi is not consistent with CUDA_VISIBLE_DEVICES
-    "profile.d/posix-config.sh".text = lib.concatLines (
-      lib.mapAttrsToList (name: value: ''
-        export ${name}="${value}"
-      '') envVars
-    );
-    "fish/config.fish".source = pkgs.writeTextFile {
-      name = "config.fish";
-      executable = true;
-      text = ''
-        set -gx PATH "/run/system-manager/sw/bin" $PATH
-      ''
-      + lib.concatLines (
-        lib.mapAttrsToList (name: value: ''
-          set -gx ${name} "${value}"
-        '') envVars
-      );
-    };
+    "profile.d/posix-config.sh".text = ''
+      export CUDA_VISIBLE_DEVICES="100"
+      export CUDA_DEVICE_ORDER="PCI_BUS_ID"
+    '';
+    "fish/config.fish".text = ''
+      fish_add_path "/run/system-manager/sw/bin"
+      set -gx CUDA_VISIBLE_DEVICES "100"
+      set -gx CUDA_DEVICE_ORDER "PCI_BUS_ID"
+    '';
   };
 }
